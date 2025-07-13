6f264e4c9048afe4225233206800e6827232ffb1
ZiplEix <ZiplEix@users.noreply.github.com>
2025-07-13 18:00:19 +0200

    Restricts CORS origins to specific URLs
    Updates allowed CORS origins to include localhost and production URLs, enhancing security by preventing unrestricted access from all origins.

--- api/main.go
+++ api/main.go
@@ -25,7 +25,7 @@ func init() {
 
 func setupCors(r *chi.Mux) {
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"*"},
+		AllowedOrigins:   []string{"http://localhost:5173", "https://scrabble.baptiste.zip"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
