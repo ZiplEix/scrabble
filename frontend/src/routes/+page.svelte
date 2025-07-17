9f220f301f3b6ab99bc3c506a41880541f9e7809
LEROYER Baptiste <ZiplEix@users.noreply.github.com>
2025-07-17 17:20:16 +0200

    Adds PWA support with manifest and installation prompt
    Integrates Progressive Web App (PWA) functionality, including a web app manifest, theme color, and installation prompt. Configures PWA plugins and assets, such as icons and manifest, in the build process. Enhances user experience by enabling the app to be installed on home screens.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,50 @@
 <script lang="ts">
 	import { user } from '$lib/stores/user';
+  	import { onMount } from 'svelte';
 	import LoginedPage from './LoginedPage.svelte';
 	import NoLoginPage from './NoLoginPage.svelte';
 
+	let deferredPrompt: any = null;
+	let canInstall = false;
+
+	onMount(() => {
+		window.addEventListener('beforeinstallprompt', (e) => {
+			e.preventDefault();
+			deferredPrompt = e;
+			canInstall = true;
+		});
+	});
+
+	async function installApp() {
+		if (deferredPrompt) {
+			deferredPrompt.prompt();
+			const { outcome } = await deferredPrompt.userChoice;
+			if (outcome === 'accepted') {
+				console.log('PWA installée');
+			}
+			deferredPrompt = null;
+			canInstall = false;
+		}
+	}
 </script>
 
 <main class="max-w-4xl mx-auto px-6 py-10">
+	{#if canInstall}
+		<div class="bg-green-100 border border-green-300 text-green-800 px-4 py-3 rounded mb-6 flex items-center justify-between shadow">
+			<div class="flex items-center gap-2">
+				<span class="text-lg">📲</span>
+				<p class="text-sm">
+					Ajoutez ce site à votre écran d’accueil pour l’utiliser comme une app.
+				</p>
+			</div>
+			<button
+				on:click={installApp}
+				class="ml-4 bg-green-600 hover:bg-green-700 text-white text-sm px-4 py-2 rounded"
+			>
+				Installer
+			</button>
+		</div>
+	{/if}
 	{#if !$user}
 		<NoLoginPage />
 	{:else}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,31 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { type SvelteKitPWAOptions } from '@vite-pwa/sveltekit'
+
+const pwaConfig: Partial<SvelteKitPWAOptions> = {
+	registerType: 'autoUpdate',
+	manifest: {
+		name: 'Scrabble Online',
+		short_name: 'Scrabble',
+		start_url: '/',
+		display: 'standalone',
+		background_color: '#ffffff',
+		theme_color: '#00a86b',
+		icons: [
+			{
+			src: '/icons/icon-192.png',
+			sizes: '192x192',
+			type: 'image/png'
+			},
+			{
+			src: '/icons/icon-512.png',
+			sizes: '512x512',
+			type: 'image/png'
+			}
+		]
+	}
+}
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
