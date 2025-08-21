4f7bed1b4dae1a5af3ca7ff3bfed490c57d63bd0
LEROYER Baptiste <ZiplEix@users.noreply.github.com>
2025-08-21 14:53:24 +0200

    Adds reusable Svelte checkbox component
    Introduces a new Svelte component that implements a customizable
    checkbox with binding support and toggle functionality. Enhances
    UI consistency and promotes code reuse by encapsulating common
    checkbox logic and styles.

---
+++ frontend/src/lib/components/Checkbox.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+    let { checked = $bindable(), onToggle }: {
+        checked: boolean,
+        onToggle: () => void
+    } = $props();
+</script>
+
+<label class="inline-flex items-center cursor-pointer">
+    <input
+        type="checkbox"
+        class="sr-only"
+        bind:checked={checked}
+        aria-label="checkbox input"
+        onchange={onToggle}
+    />
+    <div class="w-12 h-7 rounded-full relative transition-colors duration-150"
+            aria-hidden="true"
+            class:bg-green-500={checked} class:bg-gray-300={!checked}>
+        <span
+            class="absolute top-1 left-1 bg-white w-5 h-5 rounded-full shadow transition-transform duration-150"
+            style="transform-origin: center"
+            class:translate-x-5={checked}
+        ></span>
+    </div>
+</label>
