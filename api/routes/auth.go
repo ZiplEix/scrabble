3610298d5f8f1340f9ae92f818dd0e7458bfc902
LEROYER Baptiste <ZiplEix@users.noreply.github.com>
2025-08-21 14:54:17 +0200

    Removes unused 'GetMe' route from auth setup
    Eliminates the 'GetMe' route from the authentication routes as it appears redundant or no longer needed. This simplifies the route setup and reduces potential maintenance overhead.

--- api/routes/auth.go
+++ api/routes/auth.go
@@ -12,7 +12,6 @@ func setupAuthRoutes(e *echo.Echo) {
 	auth.POST("/register", controller.Register)
 	auth.POST("/login", controller.Login)
 	auth.GET("/profile", nonImplementedHandler, middleware.RequireAuth)
-	auth.GET("/me", controller.GetMe, middleware.RequireAuth)
 	auth.POST("/change-password", controller.ChangePassword, middleware.RequireAuth, middleware.RequireAdmin)
 	auth.GET("/connect-as", controller.ConnectAS, middleware.RequireAuth, middleware.RequireAdmin)
 	// auth.GET("/logout", nonImplementedHandler)
