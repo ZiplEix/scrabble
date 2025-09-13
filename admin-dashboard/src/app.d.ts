e906c155f33b8f9ca34265fd4f8da1526b7be4c3
ZiplEix <ZiplEix@users.noreply.github.com>
2025-09-13 16:09:22 +0200

    Adds user type definitions to App interfaces
    Introduces the `User` type to the `PageState` and `PageData`
    interfaces in the global `App` namespace. This ensures user
    data is properly typed and accessible across the application,
    improving type safety and maintainability.

--- admin-dashboard/src/app.d.ts
+++ admin-dashboard/src/app.d.ts
@@ -1,4 +1,7 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { User } from "./routes/dashboard/users/type";
+
 // for information about these interfaces
 declare global {
 	namespace App {
@@ -7,6 +10,14 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+
+		interface PageState {
+			user?: User;
+		}
+
+		interface PageData {
+			user: User;
+		}
 	}
 }
 
