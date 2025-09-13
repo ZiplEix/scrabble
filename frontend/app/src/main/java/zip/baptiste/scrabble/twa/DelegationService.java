566a300226f5b110b0dfca0784393da8f5594893
LEROYER Baptiste <ZiplEix@users.noreply.github.com>
2025-09-13 23:51:21 +0200

    add bubblewrap for android support

---
+++ frontend/app/src/main/java/zip/baptiste/scrabble/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package zip.baptiste.scrabble.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
